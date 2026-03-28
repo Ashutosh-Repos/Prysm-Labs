eb7261b0f2aa9e9e2b6f737ad6103f4c853170c7
Ashutosh <clashutosh04@gmail.com>
2026-03-28 22:14:46 +0530

    feat: implement main application scaffold

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Security
+  app.use(helmet());
+  app.enableCors();
+
+  // Global Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Global Exception Filters
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new AllExceptionsFilter(httpAdapter),
+    new PrismaClientExceptionFilter(),
+  );
+
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Mini CRM API')
+    .setDescription('The Mini CRM API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
